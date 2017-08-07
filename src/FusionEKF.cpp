32399973f6d319d5ccfe6b1f5de80910e3907cec
atlas604 <blitzbeat@gmail.com>
2017-08-07 04:17:09 -0400

    Add files via upload

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -43,8 +43,8 @@ FusionEKF::FusionEKF() {
   ekf_.F_ = MatrixXd(4, 4);
   ekf_.F_ << 1, 0, 1, 0,
     0, 1, 0, 1,
-		0, 0, 1, 0,
-		0, 0, 0, 1;
+    0, 0, 1, 0,
+    0, 0, 0, 1;
 
 
   ekf_.P_ = MatrixXd(4, 4);
